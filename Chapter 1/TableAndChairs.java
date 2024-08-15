2c87dcde3137b6a32d037d5e279ad0b4726fd475
ShanMoreno <johnaiken.moreno@gmail.com>
2024-08-15 17:58:16 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -5,8 +5,8 @@ System.out.println("x                        x");
 System.out.println("x                        x");
 System.out.println("x       xxxxxxxxxx       x");
 System.out.println("xxxxx x            x xxxxx");
-System.out.println("x.    x x        x x     x");
-System.out.println("x.    x x        x x     x");
+System.out.println("x     x x        x x     x");
+System.out.println("x     x x        x x     x");
 
      }
 }
